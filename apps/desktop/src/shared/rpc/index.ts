8bd45c93a26bc71f690b1ba9da324cecff0eb1bf
Elon Tang <i@blackcater.dev>
2026-03-25 20:42:54 +0800

    feat(rpc): export unified RPC API

---
+++ apps/desktop/src/shared/rpc/index.ts
@@ -0,0 +1,13 @@
+// Core types and interfaces
+export * from './types'
+export { RpcError } from './RpcError'
+export { extractRpcErrorMsg } from './utils'
+
+// Electron transport
+export { ElectronRpcServer } from './electron/ElectronRpcServer'
+export { ElectronRpcClient } from './electron/ElectronRpcClient'
+export type { WebContentsManager } from './electron/ElectronRpcServer'
+
+// HTTP transport
+export { HttpRpcServer } from './http/HttpRpcServer'
+export { HttpRpcClient } from './http/HttpRpcClient'
