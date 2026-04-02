7904b018c327c94a9a86f89518a54430ee837518
Elon Tang <i@blackcater.dev>
2026-04-02 14:03:53 +0800

    chore: changing sidebar layout

--- apps/desktop/src/renderer/src/components/app-shell/sidebar/ProjectSection.tsx
+++ apps/desktop/src/renderer/src/components/app-shell/sidebar/ProjectSection.tsx
@@ -1,16 +1,34 @@
-import { useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
 
 import { sidebarAtom } from '@renderer/atoms'
 
+import { ThreadTitleCell } from './cell/ThreadTitleCell'
 import { FlatView } from './section/FlatView'
 import { FolderView } from './section/FolderView'
 
 export function ProjectSection() {
-	const sidebar = useAtomValue(sidebarAtom)
+	const [sidebar, setSidebar] = useAtom(sidebarAtom)
 	const viewMode = sidebar.viewMode
+
+	const handleSort = () => {
+		if (viewMode === 'folder') {
+			setSidebar({ ...sidebar, viewMode: 'flat' })
+		} else if (viewMode === 'flat') {
+			setSidebar({ ...sidebar, viewMode: 'folder' })
+		}
+	}
+
+	const handleAddFolder = () => {
+		// TODO: Add Project
+	}
 
 	return (
 		<div className="flex h-full max-w-full flex-col px-2 pb-4">
+			<ThreadTitleCell
+				title="Threads"
+				onSort={handleSort}
+				onAdd={handleAddFolder}
+			/>
 			{viewMode === 'folder' ? <FolderView /> : <FlatView />}
 		</div>
 	)
