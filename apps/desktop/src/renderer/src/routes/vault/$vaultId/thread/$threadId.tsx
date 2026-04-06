fe9c3fbffa4c51fd50b43862f58e6d59713cc688
Elon Tang <i@blackcater.dev>
2026-04-07 03:06:53 +0800

    refactor(route): import Chat from features/chat

--- apps/desktop/src/renderer/src/routes/vault/$vaultId/thread/$threadId.tsx
+++ apps/desktop/src/renderer/src/routes/vault/$vaultId/thread/$threadId.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import { Chat } from '@renderer/components/chat/Chat'
+import { Chat } from '@renderer/features/chat'
 
 export const Route = createFileRoute('/vault/$vaultId/thread/$threadId')({
 	component: ThreadPage,
