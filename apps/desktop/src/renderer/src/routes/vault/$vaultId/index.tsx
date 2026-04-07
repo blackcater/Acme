9dd895b716624d6f7e00a3cb1b4e315b9c021273
Elon Tang <i@blackcater.dev>
2026-04-07 12:07:24 +0800

    fix: import path error

--- apps/desktop/src/renderer/src/routes/vault/$vaultId/index.tsx
+++ apps/desktop/src/renderer/src/routes/vault/$vaultId/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import { Chat } from '@renderer/components/chat/Chat'
+import { Chat } from '@renderer/features/chat/components/Chat'
 
 export const Route = createFileRoute('/vault/$vaultId/')({
 	component: NewThreadPage,
